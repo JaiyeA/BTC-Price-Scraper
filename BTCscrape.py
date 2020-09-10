5b992b3aac1a9eb3ab0fb128f020a356a068a893
jaiye agbonavbare <agbojai15@icloud.com>
2020-09-10 12:31:21 -0400

    parsed tag is a <span>

--- BTCscrape.py
+++ BTCscrape.py
@@ -5,6 +5,6 @@ import urllib.error
 url= 'https://cointelegraph.com/bitcoin-price-index'
 page= urllib.request.urlopen(url)
 pars= BeautifulSoup(page,'html.parser')
-price= pars.find('div',attrs={'class':'price-value'})
+price= pars.find('span',attrs={'class':'price-value'}) #the html tag was div before, but now it's span
 
 print('The price of BTC right now is '+price.text.strip())
